253e8ac6502c56909dc76ed34c07e425be392c36
tushar8871 <tusharkhachane6@gmail.com>
2020-03-22 10:55:05 +0530

    Added : toss to know which player will play

--- ticTac.sh
+++ ticTac.sh
@@ -1,8 +1,20 @@
 #!/bin/bash -x
 
+#who play first
+function whoPlayFirst(){
+	randomPlayer=$((RANDOM%2))
+	if [ $randomPlayer -eq 0 ]
+	then
+		echo "x"
+	else
+		echo "0"
+	fi
+}
+
+#reset board
 function reset(){
 	echo "Tic Tac Toe Game"
-	currentPlayer=x;
+	currentPlayer=$( whoPlayFirst $(()) );
 	gameStatus=1;
 	declare -A board;
 }
