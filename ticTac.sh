f9da4d66a5177779b3c376d60ad1147b534d8ec6
tushar8871 <tusharkhachane6@gmail.com>
2020-03-23 09:53:26 +0530

    Added : chnage turn of player and find win or tie

--- ticTac.sh
+++ ticTac.sh
@@ -1,5 +1,13 @@
-#!/bin/bash 
+#!/bin/bash
 
+#constants
+TOTALCOUNT=9;
+
+#variables
+moveCount=1;
+
+#declare array
+declare -A board
 #who play first
 function whoPlayFirst(){
 	randomPlayer=$((RANDOM%2))
@@ -15,23 +23,102 @@ function whoPlayFirst(){
 function reset(){
 	echo "Tic Tac Toe Game"
 	currentPlayer=$( whoPlayFirst $(()) );
-	gameStatus=1;
+	gameStatus=0;
 	initalize
 }
-
 #initialize board
 function initalize(){
-   for (( rowPosition=1;rowPosition<=3;rowPosition++ ))
+   for (( rowPosition=1;$rowPosition<=3;rowPosition++ ))
    do
-      echo "---------"
-      for (( columnPosition=1;columnPosition<=3;columnPosition++ ))
+      for (( columnPosition=1;$columnPosition<=3;columnPosition++ ))
       do
          board[$rowPosition,$columnPosition]="-"
       done
-      echo "| ${board[@]} |"
    done
-	echo "---------"
+	printBoard
 }
 
+
+# print board
+function printBoard(){
+	echo "---------------"
+	for (( row=1;row<=3;row++ ))
+	do
+		for (( column=1;column<=3;column++ ))
+		do
+			echo -e "| ${board[$row,$column]} | \c"
+		done
+		echo
+	done
+	echo "---------------"
+}
+
+#change player
+function changePlayer(){
+	if [[ $1 == "x" ]]
+	then
+		currentPlayer="0";
+	else
+		currentPlayer="x";
+	fi
+}
+
+#check win
+function checkWin(){
+	gameStatus=0;
+	for (( i=1;i<=3;i++ ))
+	do
+		if [[ ${board[$i,1]} == $currentPlayer && ${board[$i,1]} == ${board[$i,2]} && ${board[$i,1]} == ${board[$i,3]} ]]
+		then
+			gameStatus=1;
+		fi
+		if [[ ${board[1,$i]} == $currentPlayer && ${board[1,$i]} == ${board[2,$i]} && ${board[1,$i]} == ${board[3,$i]} ]]
+		then
+			gameStatus=1;
+		fi
+	done
+	
+	if [[ ${board[1,1]} == $currentPlayer &&  ${board[1,1]} == ${board[2,2]} && ${board[1,1]} == ${board[3,3]} ]]
+	then
+		gameStatus=1;
+	fi
+	
+	if [[ ${board[1,3]} == $currentPlayer && ${board[1,3]} == ${board[2,2]} && ${board[1,3]} == ${board[3,1]} ]]
+	then
+		gameStatus=1;
+	fi
+	
+	echo $gameStatus
+}
+
+#write mark on board
+function placeMark(){
+	if [[ ${board[$1,$2]} == - ]]
+	then
+		board[$1,$2]=$currentPlayer
+		printBoard
+		if [[ $( checkWin $(()) ) -eq 1 ]]
+		then
+			echo "$currentPlayer wins"
+			exit
+		fi
+		changePlayer $currentPlayer
+	else
+		echo "Already occupied"
+	fi
+}
+
+#start execution
 reset
-echo $currentPlayer
+while [[ $moveCount -le $TOTALCOUNT ]]
+do
+	read -p "Enter row " row
+	read -p "Enter column " column
+	placeMark $row $column
+	((moveCount++)) 
+done
+
+if [[ $gameStatus -eq 0 ]]
+then
+	echo "Match tie ! "
+fi 
